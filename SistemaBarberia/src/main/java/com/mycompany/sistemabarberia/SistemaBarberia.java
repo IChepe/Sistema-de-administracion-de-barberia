9c281fbe6e64529338d570d055fc065ca5aefea9
Jose David Astorga Mora <jdastorgamo1782@gmail.com>
2026-07-26 13:44:46 -0600

    Implementacion inicial del proyecto

---
+++ SistemaBarberia/src/main/java/com/mycompany/sistemabarberia/SistemaBarberia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemabarberia;
+
+/**
+ *
+ * @author josed
+ */
+public class SistemaBarberia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
