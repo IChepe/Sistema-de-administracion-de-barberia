630e2c82e88dd4a0ea0496c4fc4d41728f928e63
pc <pc@192.168.0.15>
2026-08-11 03:58:05 -0600

    Inventario

--- SistemaBarberia/src/main/java/com/mycompany/sistemabarberia/Clases/Inventario.java
+++ SistemaBarberia/src/main/java/com/mycompany/sistemabarberia/Clases/Inventario.java
@@ -1,23 +1,26 @@
 package com.mycompany.sistemabarberia.Clases;
 
+import com.mycompany.sistemabarberia.Enumeradores.Categoria;
+
 public class Inventario {
+ 
     private int id;
-    private String nombre;
     private String descripcion;
-    private Categoria categorias;
+    private Categoria categoria;
     private int stock;
     private int stockMinimo;
     private double precioCompra;
     private double precioVenta;
     private String unidadMedida;
 
-    
-    public Inventario(int id, String nombre, String descripcion, Categoria categorias,
-                      int stock, int stockMinimo, double precioCompra, double precioVenta, String unidadMedida) {
+    public Inventario() {
+    }
+
+    public Inventario(int id, String descripcion, Categoria categoria, int stock, int stockMinimo,
+                      double precioCompra, double precioVenta, String unidadMedida) {
         this.id = id;
-        this.nombre = nombre;
         this.descripcion = descripcion;
-        this.categorias = categorias;
+        this.categoria = categoria;
         this.stock = stock;
         this.stockMinimo = stockMinimo;
         this.precioCompra = precioCompra;
@@ -25,84 +28,81 @@ public class Inventario {
         this.unidadMedida = unidadMedida;
     }
 
-    
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
-
-    public String getDescripcion() { return descripcion; }
-    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
-
-    public Categoria getCategorias() { return categorias; }
-    public void setCategorias(Categoria categorias) { this.categorias = categorias; }
-
-    public int getStock() { return stock; }
-    public void setStock(int stock) { this.stock = stock; }
-
-    public int getStockMinimo() { return stockMinimo; }
-    public void setStockMinimo(int stockMinimo) { this.stockMinimo = stockMinimo; }
-
-    public double getPrecioCompra() { return precioCompra; }
-    public void setPrecioCompra(double precioCompra) { this.precioCompra = precioCompra; }
-
-    public double getPrecioVenta() { return precioVenta; }
-    public void setPrecioVenta(double precioVenta) { this.precioVenta = precioVenta; }
-
-    public String getUnidadMedida() { return unidadMedida; }
-    public void setUnidadMedida(String unidadMedida) { this.unidadMedida = unidadMedida; }
-
-   
-    public void crear() {
-        System.out.println("Producto creado: " + nombre);
+    // Getters
+    public int getId() {
+        return id;
     }
 
-    public void consultar() {
-        System.out.println("Consulta de producto: " + this);
+    public String getDescripcion() {
+        return descripcion;
     }
 
-    public void actualizar(String nuevoNombre, String nuevaDescripcion, double nuevoPrecioVenta) {
-        this.nombre = nuevoNombre;
-        this.descripcion = nuevaDescripcion;
-        this.precioVenta = nuevoPrecioVenta;
-        System.out.println("Producto actualizado: " + nombre);
+    public Categoria getCategoria() {
+        return categoria;
     }
 
-    public void eliminar() {
-        System.out.println("Producto eliminado: " + nombre);
+    public int getStock() {
+        return stock;
     }
 
-    public void actualizarStock(int nuevoStock) {
-        this.stock = nuevoStock;
-        System.out.println("Stock actualizado: " + stock);
+    public int getStockMinimo() {
+        return stockMinimo;
     }
 
-    public boolean verificarStockMinimo() {
-        return stock <= stockMinimo;
+    public double getPrecioCompra() {
+        return precioCompra;
     }
 
-    public void descontarStock(int cantidad) {
-        if (cantidad <= stock) {
-            stock -= cantidad;
-            System.out.println("Stock descontado. Nuevo stock: " + stock);
-        } else {
-            System.out.println("Error: cantidad a descontar mayor que el stock disponible.");
-        }
+    public double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public String getUnidadMedida() {
+        return unidadMedida;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public void setStockMinimo(int stockMinimo) {
+        this.stockMinimo = stockMinimo;
+    }
+
+    public void setPrecioCompra(double precioCompra) {
+        this.precioCompra = precioCompra;
+    }
+
+    public void setPrecioVenta(double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public void setUnidadMedida(String unidadMedida) {
+        this.unidadMedida = unidadMedida;
     }
 
     @Override
     public String toString() {
-        return "Inventario{" +
-                "id=" + id +
-                ", nombre='" + nombre + '\'' +
-                ", descripcion='" + descripcion + '\'' +
-                ", categoria=" + (categorias != null ? categorias.getNombre() : "Sin categoría") +
-                ", stock=" + stock +
-                ", stockMinimo=" + stockMinimo +
-                ", precioCompra=" + precioCompra +
-                ", precioVenta=" + precioVenta +
-                ", unidadMedida='" + unidadMedida + '\'' +
-                '}';
+        return "Producto ID: " + id +
+               "\nDescripción: " + descripcion +
+               "\nCategoría: " + categoria +
+               "\nStock: " + stock +
+               "\nStock mínimo: " + stockMinimo +
+               "\nPrecio compra: " + precioCompra +
+               "\nPrecio venta: " + precioVenta +
+               "\nUnidad de medida: " + unidadMedida;
     }
 }
